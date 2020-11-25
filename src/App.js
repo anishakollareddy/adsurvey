56ae2529e1e45064f4cc05bae67e12ac0b3fbfea
Anisha <bluestar225@gmail.com>
2020-11-24 21:09:51 -0800

    added survey

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import * as Survey from 'survey-react'
 import './App.css';
 import Title from './Title';
+import "survey-react/survey.css";
+
 
 const sendDataToServer = (survey) => {
   // var resultAsString = JSON.stringify(survey.data);
-  survey.sendResult('53686cf7-ae4d-4339-a535-dd33507b0f16'); // unique id tied to survey created on surveyjs.io
+  survey.sendResult('63376b27-fb10-486c-8434-f98223fcda09'); // unique id tied to survey created on surveyjs.io
   // alert(resultAsString); //send Ajax request to your web server.
 }
 
@@ -16,8 +18,10 @@ const sendDataToServer = (survey) => {
 class App extends Component {
   render() {
     Survey.Survey.cssType = "bootstrap";
-    const model = new Survey.Model({surveyId: '0320a725-4523-4536-bf21-91febbfd2eca'}); // surveyId is pulled from online survey created on surveyjs.io. The model is imported.
+    // Survey.Survey.StylesManager.applyTheme("modern");
 
+    const model = new Survey.Model({surveyId: 'cde59f7b-13cf-43d2-ac21-77d7330465e6'}); // surveyId is pulled from online survey created on surveyjs.io. The model is imported.
+    
     return (
       <div className="App">
         <Title />
