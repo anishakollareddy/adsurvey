814de4c6992d26982d9f0e5ca7d4dfb58f5e5c8f
Anisha <bluestar225@gmail.com>
2020-11-24 20:50:50 -0800

    woieghwoe

--- src/App.js
+++ src/App.js
@@ -20,8 +20,6 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
